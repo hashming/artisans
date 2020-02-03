5b5291d59bd378351f72ca483cd0f8da7601fbc1
e <haoduoduoyu@163.com>
2020-02-03 20:00:31 +0800

    引入配置文件，存储一些可以变化的信息，通过@value注解来进行动态的调用。 这里配置文件中添加的是id、secret、uri

--- src/main/java/com/duoduo/hashming/artisan/controller/AuthorizeController.java
+++ src/main/java/com/duoduo/hashming/artisan/controller/AuthorizeController.java
@@ -1,8 +1,10 @@
 package com.duoduo.hashming.artisan.controller;
 
 import com.duoduo.hashming.artisan.dto.AccessTokenDTO;
+import com.duoduo.hashming.artisan.dto.GithubUser;
 import com.duoduo.hashming.artisan.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,16 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    /**
+     * value注解连接到配置文件，从配置文件中去读取数据
+     */
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String secret;
+    @Value("${github.redirect.uri}")
+    private String uri;
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
@@ -19,13 +31,15 @@ public class AuthorizeController {
         //录入code和state
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();//承载类
 
-        accessTokenDTO.setClient_id("c69ece8b7824522367e0");
-        accessTokenDTO.setClient_secret("58f2c0643fd18fe045b876a0c4e245ac347f2b59");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(secret);
         accessTokenDTO.setState(state);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");//应用程序中用户经过授权后发送的 URL 就是登录成功后跳转的页面
+        accessTokenDTO.setRedirect_uri(uri);//应用程序中用户经过授权后发送的 URL 就是登录成功后跳转的页面
 
-        githubProvider.getAccessToken(accessTokenDTO);//调用accesstoken这个方法把accesstoken承载类传入这个方法
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);//调用accesstoken这个方法把accesstoken承载类传入这个方法
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
 
     }
