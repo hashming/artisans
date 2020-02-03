300d1d07a3670e3eb5ac595cc0ad5d18ecdc2239
e <haoduoduoyu@163.com>
2020-02-03 08:16:12 +0800

    加入accesstoken认证

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ spring社区：https://spring.io/guides
 https://spring.io/guides/gs/serving-web-content/
 
 参考地址：http://localhost:8080/greeting?name=User
+
+github:https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+
+bootstrap:https://v3.bootcss.com/components/#navbar-buttons
+
 ## 工具
 
 git工具
---
+++ src/main/java/com/duoduo/hashming/artisan/dto/AccessTokenDTO.java
@@ -0,0 +1,50 @@
+package com.duoduo.hashming.artisan.dto;
+
+public class AccessTokenDTO {
+    private String client_id;
+    private String clident_secret;
+    private String code;
+    private String redirect_uri;
+    private String state;
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClident_secret() {
+        return clident_secret;
+    }
+
+    public void setClident_secret(String clident_secret) {
+        this.clident_secret = clident_secret;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getRedirect_uri() {
+        return redirect_uri;
+    }
+
+    public void setRedirect_uri(String redirect_uri) {
+        this.redirect_uri = redirect_uri;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
